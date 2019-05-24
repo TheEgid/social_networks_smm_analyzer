1a6efe37385bb6a4cf56ba97f21c75f81f32d9e3
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-05-24 20:31:54 +0300

    Rebuild VK module

--- social_networks_smm_analyzer/vk_analyze.py
+++ social_networks_smm_analyzer/vk_analyze.py
@@ -12,30 +12,32 @@ class VKApiResponseError(Exception):
 def get_vk_group_id(token, group_name):
     params = {'group_ids': group_name, 'access_token': token, 'v': 5.95}
     response = requests.get('https://api.vk.com/method/groups.getById',
-                            params=params)
-    if response.json().get('error'):
-        _msg = response.json()['error']['error_msg']
+                            params=params).json()
+    if response.get('error'):
+        _msg = response['error']['error_msg']
         raise(VKApiResponseError(_msg))
-    vk_group_id = -1 * response.json()['response'][0]['id']  # with "-" (vk.com/dev/wall.post#owner_id)
+    vk_group_id = -1 * response['response'][0]['id']  # with "-" (vk.com/dev/wall.post#owner_id)
     return vk_group_id
 
 
 @storage_json_io_decorator('test_data', 'all_vk_posts.json')
-def get_vk_posts(uid, token, pages_limit=100):
-    vk_post_ids = []
+def get_vk_posts(uid, token, pages_limit=10000):
+    vk_posts = []
+    vk_count = 100
     _url = "https://api.vk.com/method/wall.get"
-    params = {'access_token': token, 'owner_id': uid, 'v': 5.95, 'count': 100}
-    for page_numbers in count(start=0, step=pages_limit):
-        params['offset'] = page_numbers
-        response = requests.get(_url, params=params)
-        if response.json().get('error'):
-            _msg = response.json()['error']['error_msg']
-            raise(VKApiResponseError(_msg))
-        for result in response.json()['response']['items']:
-            vk_post_ids.append(result)
-        if page_numbers >= pages_limit:
+    params = {'access_token': token, 'owner_id': uid, 'v': 5.95, 'count': vk_count}
+    for page_number in count(start=0, step=vk_count):
+        params['offset'] = page_number
+        response = requests.get(_url, params=params).json()
+        if response.get('error'):
+            raise(VKApiResponseError(response['error']['error_msg']))
+        for post in response['response']['items']:
+            vk_posts.append([post['id'], post['comments']['count'],
+                                post['likes']['count']])
+        if page_number >= pages_limit or \
+                page_number >= response['response']['count']:
             break
-    return [vk_post['id'] for vk_post in vk_post_ids]
+    return vk_posts
 
 
 def add_vk_comments_threads(vk_comments_list):
@@ -52,26 +54,26 @@ def add_vk_comments_threads(vk_comments_list):
     return all_comments_list
 
 
-def get_vk_comments_from_post_id(vk_post_id, uid, token, pages_limit=100):
+def get_vk_comments_from_post(vk_post, uid, token, pages_limit=1000):
+    post_id, comments_count, likes_count = vk_post
     vk_comments = []
     _url = "https://api.vk.com/method/wall.getComments"
     params = {'access_token': token, 'owner_id': uid, 'count': 100, 'v': 5.95,
-              'post_id': vk_post_id, 'extended': 1, 'thread_items_count': 10}
-    for page_numbers in count(start=0, step=pages_limit):
-        params['offset'] = page_numbers
-        response = requests.get(_url, params=params)
-        if response.json().get('error'):
-            _msg = response.json()['error']['error_msg']
-            raise(VKApiResponseError(_msg))
-        for result in response.json()['response']['items']:
+              'post_id': post_id, 'extended': 1, 'thread_items_count': 10}
+    for page_number in count(start=0, step=pages_limit):
+        params['offset'] = page_number
+        response = requests.get(_url, params=params).json()
+        if response.get('error'):
+            raise(VKApiResponseError(response['error']['error_msg']))
+        for result in response['response']['items']:
             vk_comments.append(result)
-        if page_numbers >= pages_limit:
+        if page_number >= pages_limit or page_number >= comments_count:
             break
-    all_vk_comments_from_post_id = add_vk_comments_threads(vk_comments)
-    return all_vk_comments_from_post_id
+    all_vk_comments_from_post = add_vk_comments_threads(vk_comments)
+    return all_vk_comments_from_post
 
 
-def get_vk_commentator_who_commited_last_weeks(comments_dict, weeks):
+def get_vk_commentator_who_commited_last_weeks(comments_dict, weeks=2):
     qty_days = weeks * 7
     now = datetime.datetime.now()
     past_time_point = now - datetime.timedelta(qty_days)
@@ -81,29 +83,29 @@ def get_vk_commentator_who_commited_last_weeks(comments_dict, weeks):
                 return comment_author_id
 
 
-def get_vk_likers_from_post_id(post_id, uid, token, pages_limit=1000):
+def get_vk_likers_from_post(vk_post, uid, token, pages_limit=1000):
+    post_id, comments_count, likes_count = vk_post
     vk_likers = []
     _url = "https://api.vk.com/method/likes.getList"
     params = {'access_token': token, 'owner_id': uid, 'v': 5.95, 'count': 1000,
               'item_id': post_id, 'type': 'post', 'skip_own': 1}
-    for page_numbers in count(start=0, step=pages_limit):
-        params['offset'] = page_numbers
-        response = requests.get(_url, params=params)
-        if response.json().get('error'):
-            _msg = response.json()['error']['error_msg']
-            raise(VKApiResponseError(_msg))
-        for result in response.json()['response']['items']:
+    for page_number in count(start=0, step=pages_limit):
+        params['offset'] = page_number
+        response = requests.get(_url, params=params).json()
+        if response.get('error'):
+            raise(VKApiResponseError(response['error']['error_msg']))
+        for result in response['response']['items']:
             vk_likers.append(result)
-        if page_numbers >= pages_limit:
+        if page_number >= pages_limit or page_number >= likes_count:
             break
     return set(vk_likers)
 
 
 @storage_json_io_decorator('test_data', 'all_vk_last_weeks_commentators.json')
-def get_vk_last_weeks_commentators(vk_post_ids, uid, token, weeks):
+def get_vk_last_weeks_commentators(vk_posts, uid, token, weeks):
     last_weeks_commentators = []
-    for post_id in vk_post_ids:
-        vk_comments = get_vk_comments_from_post_id(post_id, uid, token)
+    for post in vk_posts:
+        vk_comments = get_vk_comments_from_post(post, uid, token)
         last_weeks_commentators.extend(
             [get_vk_commentator_who_commited_last_weeks(comments, weeks) for
              comments in vk_comments])
@@ -111,7 +113,7 @@ def get_vk_last_weeks_commentators(vk_post_ids, uid, token, weeks):
 
 
 @storage_json_io_decorator('test_data', 'all_vk_likers.json')
-def get_all_vk_likers(vk_post_ids, uid, token):
-    all_likers = [get_vk_likers_from_post_id(post_id, uid, token)
-                  for post_id in vk_post_ids]
+def get_all_vk_likers(vk_posts, uid, token):
+    all_likers = [get_vk_likers_from_post(post, uid, token)
+                  for post in vk_posts]
     return list(set(chain.from_iterable(all_likers)))
